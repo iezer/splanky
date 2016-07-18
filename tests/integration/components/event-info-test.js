8234e01b25808766e5ca20884ddfd555305ea8a7
Isaac Ezer <isaac.ezer@gmail.com>
2016-07-17 22:19:57 -0400

    fix select artist test

--- tests/integration/components/event-info-test.js
+++ tests/integration/components/event-info-test.js
@@ -8,6 +8,10 @@ moduleForComponent('event-info', 'Integration | Component | event info', {
   integration: true,
   beforeEach() {
     setupMirage(this);
+    this.artist = { id: 'monk', name: 'Monk' };
+    this.event = { startTime: new Date(), artists: [ this.artist ] };
+    this.set('event', this.event);
+    this.on('selectArtist', () => this);
   },
   afterEach() {
     teardownMirage(this);
@@ -17,32 +21,25 @@ moduleForComponent('event-info', 'Integration | Component | event info', {
 test('it renders an event', function(assert) {
   assert.expect(2);
 
-  let event = {
-    startTime: new Date(),
-    name: 'Monk'
-  };
-
-  this.set('event', event);
-  this.render(hbs`{{event-info event=event}}`);
-
+  this.render(hbs`{{event-info event=event selectArtist=(action 'selectArtist')}}`);
   let text = this.$().text().trim();
   assert.notStrictEqual(text.indexOf('Monk'), -1, 'shows event name');
-  assert.notStrictEqual(text.indexOf(event.startTime.toDateString()), -1, 'shows event date');
+  assert.notStrictEqual(text.indexOf(this.event.startTime.toDateString()), -1, 'shows event date');
 });
 
 test('it renders artists for event and handles action', function(assert) {
-  assert.expect(3);
-  let artist = { name: 'Monk' };
-  let event = { startTime: new Date(), artists: [ artist ] };
+  assert.expect(4);
 
-  this.on('selectArtist', function(param) {
-    assert.deepEqual(artist, param, 'artist sent to action on click');
+  this.set('selectedArtist', null);
+  this.on('selectArtist', param => {
+    assert.deepEqual(this.artist, param, 'artist sent to action on click');
+    this.set('selectedArtist', param);
   });
 
-  this.set('event', event);
-  this.render(hbs`{{event-info event=event selectArtist=(action 'selectArtist')}}`);
+  this.render(hbs`{{event-info event=event selectArtist=(action 'selectArtist') selectedArtist=selectedArtist}}`);
 
   assert.is$('.event-info__artist:contains(Monk)', 'shows artist name');
+  assert.notOk(this.$('.event-info__artist.selected:contains(Monk)').length, 'artist initially not selected');
   this.$('.event-info__artist').click();
   assert.is$('.event-info__artist.selected:contains(Monk)', 'artist name is selected');
 });
