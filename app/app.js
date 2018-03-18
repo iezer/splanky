9bb8b09dc98a5529278351672fc8f93d4b3ca07d
Isaac Ezer <isaac.ezer@gmail.com>
2018-03-18 19:14:54 -0400

    fix tests

--- app/app.js
+++ app/app.js
@@ -1,12 +1,9 @@
-import Ember from 'ember';
 import Application from '@ember/application';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
 let App;
-
-Ember.MODEL_FACTORY_INJECTIONS = true; // eslint-disable-line ember-rules/destructure-namespaces
 
 App = Application.extend({
   modulePrefix: config.modulePrefix,
