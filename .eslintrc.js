0cd78add84755244b9e001b01ce794c469c49a76
Isaac Ezer <isaac.ezer@gmail.com>
2019-07-02 13:42:30 +0200

    ember 3.10

--- .eslintrc.js
+++ .eslintrc.js
@@ -36,16 +36,15 @@ module.exports = {
       env: {
         browser: false,
         node: true
-      }
-    },
+      },
+      plugins: ['node'],
+      rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
+        // add your custom rules and overrides for node files here
 
-    // test files
-    {
-      files: ['tests/**/*.js'],
-      excludedFiles: ['tests/dummy/**/*.js'],
-      env: {
-        embertest: true
-      }
+        // this can be removed once the following is fixed
+        // https://github.com/mysticatea/eslint-plugin-node/issues/77
+        'node/no-unpublished-require': 'off'
+      })
     }
   ]
 };
