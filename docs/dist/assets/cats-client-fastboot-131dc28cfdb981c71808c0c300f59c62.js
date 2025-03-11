b494c53c75e8f8524f02f6e5cc1f6766c4430ed2
Isaac Ezer <isaac.ezer@gmail.com>
2025-03-11 20:53:54 +0000

    docs

---
+++ docs/dist/assets/cats-client-fastboot-131dc28cfdb981c71808c0c300f59c62.js
@@ -0,0 +1,15 @@
+define("cats-client/initializers/ajax",["exports"],function(e){"use strict"
+Object.defineProperty(e,"__esModule",{value:!0}),e.default=void 0
+var t=Ember.get,r=function(e){var r=t(this,"fastboot.request.protocol")
+if(/^\/\//.test(e.url))e.url=r+e.url
+else if(!/^https?:\/\//.test(e.url))try{e.url=r+"//"+t(this,"fastboot.request.host")+e.url}catch(o){throw new Error("You are using Ember Data with no host defined in your adapter. This will attempt to use the host of the FastBoot request, which is not configured for the current host of this request. Please set the hostWhitelist property for in your environment.js. FastBoot Error: "+o.message)}if(!najax)throw new Error("najax does not seem to be defined in your app. Did you override it via `addOrOverrideSandboxGlobals` in the fastboot server?")
+najax(e)},o={name:"ajax-service",initialize:function(e){e.register("ajax:node",r,{instantiate:!1}),e.inject("adapter","_ajaxRequest","ajax:node"),e.inject("adapter","fastboot","service:fastboot")}}
+e.default=o}),define("cats-client/initializers/error-handler",["exports"],function(e){"use strict"
+Object.defineProperty(e,"__esModule",{value:!0}),e.default=void 0
+var t={name:"error-handler",initialize:function(e){Ember.onerror||(Ember.onerror=function(e){var t="There was an error running your app in fastboot. More info about the error: \n ".concat(e.stack||e)
+Ember.Logger.error(t)})}}
+e.default=t}),define("cats-client/instance-initializers/ember-data-fastboot",["exports"],function(e){"use strict"
+function t(e){var t=e.lookup("service:store"),r=e.lookup("service:fastboot").get("shoebox"),o=e.lookup("data-adapter:main").getModelTypes().mapBy("name")
+r.put("ember-data-store",{get records(){return o.map(function(e){return t.peekAll(e).toArray()}).reduce(function(e,t){return e.concat(t)},[]).filter(function(e){return e.get("isLoaded")&&!e.get("isNew")}).map(function(e){return e.serialize({includeId:!0})}).reduce(function(e,t){return e.data.push(t.data),e},{data:[]})}})}Object.defineProperty(e,"__esModule",{value:!0}),e.initialize=t,e.default=void 0
+var r={name:"ember-data-fastboot",initialize:t}
+e.default=r})
